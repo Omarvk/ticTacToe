173fc03f9434ddcfa7fe4dd517399c46b0c02d1d
Laura Orsini Franca <laurar14@i-7f3b5dd9.csd07cloud.internal>
2015-10-23 16:11:44 +0000

    Player Class added on new branch

---
+++ src/main/java/com/btfc/tictactoe/Player.java
@@ -0,0 +1,23 @@
+public class Player
+{
+        String name;
+        int playerScore;
+
+        public void setName(String n)
+        {
+                name = n;
+        }
+        public String getName(String n)
+        {
+                return n;
+        }
+
+	public void setScore(int n)
+        {
+                playerScore = n;
+        }
+        public int Score(int n)
+        {
+                return n;
+        }
+}

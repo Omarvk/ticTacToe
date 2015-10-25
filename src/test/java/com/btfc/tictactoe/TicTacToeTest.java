6f83389e0ec67efd3e25610685e2a0d3a161234e
brah <steinibragason@gmail.com>
2015-10-25 16:37:35 +0000

    Tests for isgamewon added

--- src/test/java/com/btfc/tictactoe/TicTacToeTest.java
+++ src/test/java/com/btfc/tictactoe/TicTacToeTest.java
@@ -18,6 +18,7 @@ public class TicTacToeTest {
         TicTacToe game = new TicTacToe();
 	int[][] gameField = new int[game.getXDimension()][game.getYDimension()];
 	int[][] nonEmptyField = new int[game.getXDimension()][game.getYDimension()];
+	//NOTE: This is dirty but inserting the same filling algorithm here seems dum
 	int[][] emptyField = {{32, 32, 32}, {32, 32, 32}, {32, 32, 32}};
 	//insert a couple of random characters into random positions in the array
 	for(int i = 0; i < 10; i++)
@@ -37,7 +38,29 @@ public class TicTacToeTest {
     @Test
     public void testIsGameWon()
     {
-        //We can't actually perform moves yet  
-	assertEquals(1, 1);
+        final int X = 88;
+        final int Y = 79;
+	final int E = 32; //shorthand for empty 
+        TicTacToe game = new TicTacToe();
+	//create a bunch of scenarios
+	int[][] wonGame1 = {{X,X,X}, {E,E,E}, {E,E,E}};
+	int[][] wonGame2 = {{X,E,X}, {Y,X,Y}, {X,E,E}};
+	int[][] wonGame3 = {{X,X,X}, {E,X,E}, {Y,Y,Y}};
+	int[][] notWonGame1 = {{E,E,E}, {E,E,E}, {E,E,E}};
+	int[][] notWonGame2 = {{E,X,E}, {X,E,X}, {E,X,E}};
+	int[][] notWonGame3 = {{X,Y,X}, {X,Y,Y}, {Y,X,Y}};
+	//set the game field to our scenarios and ensure we are getting the right results
+	game.setField(wonGame1);
+	assertEquals(true, game.isGameWon());
+	game.setField(wonGame2);
+	assertEquals(true, game.isGameWon());
+	game.setField(wonGame2);
+	assertEquals(true, game.isGameWon());
+	game.setField(notWonGame1);
+	assertEquals(false, game.isGameWon());
+	game.setField(notWonGame3);
+	assertEquals(false, game.isGameWon());
+	game.setField(notWonGame3);
+	assertEquals(false, game.isGameWon());
     }
 }

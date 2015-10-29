d00b826152a6078696814270cf1f73aeff253c9e
SigurdurBaldursson <sigurdur14@i-7f3b5dd9.csd07cloud.internal>
2015-10-29 15:20:03 +0000

    Service.java class added

---
+++ src/main/java/com/btfc/tictactoe/Service.java
@@ -0,0 +1,25 @@
+package com.btfc.tictactoe;
+
+public class Service
+{
+    private Repository repo;
+
+    Service(){
+	this.repo = new Repository();
+    }
+    
+    void addPlayer(Player p){
+	repo.addPlayer(p);
+    }
+    
+    void addPoints(Player p, int score){
+	
+	repo.addPoints(p, score);
+    }
+
+
+
+
+
+
+}

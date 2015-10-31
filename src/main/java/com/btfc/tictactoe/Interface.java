dedd887b0d9ab62038e8e4bbb672f7f5038a1755
Guðmundur Ellert <gudmundur.ellert@gmail.com>
2015-10-31 17:19:00 +0000

    Interface printing out test string

--- src/main/java/com/btfc/tictactoe/Interface.java
+++ src/main/java/com/btfc/tictactoe/Interface.java
@@ -1,14 +1,13 @@
 //This is the console interface class expected to be replaced later with web gui
-
-
 package com.btfc.tictactoe;
 
 public class Interface 
 {
-    public String  menu()
+    public void  printMenu()
     {
-      return ("Hello World");
+      System.out.print("Hello World");
     }
+
 }
 
 
@@ -23,6 +22,14 @@ public class Interface
 	//remove trailing newline character
 	System.in.read();
 	System.out.println(x + ", " + y);
+
+
+	
+     System.out.println("Welcome to Tic Tac Toe");
+     System.out.println("----------------------");
+     System.out.println("1. For new game ");
+     System.out.println("2. To exit game ");
+     System.out.println("----------------------");
 */
 
 

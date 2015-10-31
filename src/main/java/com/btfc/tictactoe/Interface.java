3a8b9df4752e79c59a10968021364f9242092101
Guðmundur Ellert <gudmundur.ellert@gmail.com>
2015-10-31 16:13:54 +0000

    Interface menu() created

---
+++ src/main/java/com/btfc/tictactoe/Interface.java
@@ -0,0 +1,30 @@
+//This is the console interface class expected to be replaced later with web gui
+
+
+package com.btfc.tictactoe;
+
+public class Interface 
+{
+    public String  menu()
+    {
+      return ("Hello World");
+    }
+}
+
+
+/*
+
+	System.out.println("Enter the x, y coordinates you want move to");
+	x = (System.in.read() - 48);
+	//clear out the comma and space
+	System.in.read();
+	System.in.read();
+	y = (System.in.read() - 48);
+	//remove trailing newline character
+	System.in.read();
+	System.out.println(x + ", " + y);
+*/
+
+
+
+

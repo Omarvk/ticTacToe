12c96950c393e261aa6c11b5ac44ad8dfc600841
Guðmundur Ellert <gudmundur.ellert@gmail.com>
2015-11-01 15:39:29 +0000

    test for enter player name

--- src/test/java/com/btfc/tictactoe/InterfaceTest.java
+++ src/test/java/com/btfc/tictactoe/InterfaceTest.java
@@ -1,8 +1,9 @@
 //This is a test TDD class for Interface.java
 package com.btfc.test;
-
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.Before;
@@ -35,7 +36,24 @@ public class InterfaceTest
 	  i.playerWins("Guest");
 	  assertEquals("Guest wins!", outContent.toString());
 	}
-
+	
+	@Test
+	public void testEnterPlayerName()
+	{
+	  try 
+	  {
+	    ByteArrayInputStream in = new ByteArrayInputStream("test1\n".getBytes());
+	    System.setIn(in);
+	    Interface i = new Interface();
+	    String test = i.enterPlayerName();
+	    assertEquals("test1", test );
+	  }
+	  catch(IOException e)
+	  {
+	    System.err.println("Caught IOException: "); 
+	  //ehh
+	  }
+	}
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 

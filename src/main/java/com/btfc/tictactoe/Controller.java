80721aed31092eb1df9c1a886766e87dad0516ac
Omar <killerbie@i-7f3b5dd9.csd07cloud.internal>
2015-11-01 01:03:58 +0000

    Spark er gaman...

--- src/main/java/com/btfc/tictactoe/Controller.java
+++ src/main/java/com/btfc/tictactoe/Controller.java
@@ -1,19 +1,27 @@
 package com.btfc.tictactoe;
 
-
+import java.util.*;
 import spark.*;
+import spark.Route;
 import static spark.Spark.*;
 import static com.btfc.tictactoe.JsonUtil.*;
-
 public class Controller {
+	private static Player players = new Player();
+	private static Map<String, Player> playa = new HashMap<String, Player>();
 	public Controller(final PlayerService player) {
-	
-		//post("/fullname", (req, res) -> player.getPlayer());
-		post("/name", (req, res) -> { 
-		////	req.queryParams("name")
 		
-			res.status(200);
-			return player.getPlayer();
+		//post("/fullname", (req, res) -> player.getPlayer());
+		post("/name",new Route(){ 
+		////	req.queryParams("name")
+			@Override
+			public Object handle(Request request, Response response)
+			{
+				String name = request.params("name");
+				String score = "0";
+				player.setPlayer(name);
+			    playa.put("1", player.getPlayer());
+				return "PlayerName: " + playa;
+			}
 		});
 		/*after((req, res) -> {
 			res.type("application/json");

5e3435afb9fec5600ff7f823598da6faec332051
SigurdurBaldursson <sigurdur14@i-7f3b5dd9.csd07cloud.internal>
2015-11-03 14:23:14 +0000

    Finishing up tictaclogictests

--- src/test/java/com/btfc/tictactoe/TicTacLogicTest.java
+++ src/test/java/com/btfc/tictactoe/TicTacLogicTest.java
@@ -31,33 +31,109 @@ public class TicTacLogicTest{
     }
     **/
     @Test
-    public void testIsGameWon()
+    public void testIsGameWonHorizontalFirstLine()
     {
-        final int X = 88;
-        final int Y = 79;
-        final int E = 32; //shorthand for empty                                                        
-        TicTacToe game = new TicTacToe();
-        //create a bunch of scenarios                                                                  
-        int[][] wonGame1 = {{X,X,X}, {E,E,E}, {E,E,E}};
-        int[][] wonGame2 = {{X,E,X}, {Y,X,Y}, {X,E,E}};
-        int[][] wonGame3 = {{X,X,X}, {E,X,E}, {Y,Y,Y}};
-        int[][] notWonGame1 = {{E,E,E}, {E,E,E}, {E,E,E}};
-        int[][] notWonGame2 = {{E,X,E}, {X,E,X}, {E,X,E}};
-        int[][] notWonGame3 = {{X,Y,X}, {X,Y,Y}, {Y,X,Y}};
-        //set the game field to our scenarios and ensure we are getting the right results              
-        game.setField(wonGame1);
-        assertEquals(true, game.isGameWon());
-        game.setField(wonGame2);
-        assertEquals(true, game.isGameWon());
-        game.setField(wonGame2);
-        assertEquals(true, game.isGameWon());
-        game.setField(notWonGame1);
-        assertEquals(false, game.isGameWon());
-        game.setField(notWonGame3);
-        assertEquals(false, game.isGameWon());
-        game.setField(notWonGame3);
-        assertEquals(false, game.isGameWon());
+	final int X = 88;
+	final int Y = 79;
+	final int E = 32; //shorthand for empty                                                        
+	TicTacLogic game = new TicTacLogic();
+	int[][] wonGame1 = {{X,X,X}, {E,E,E}, {E,E,E}};  
+	game.board.setField(wonGame1);
+	assertEquals(true, game.isGameWon());
+	
+	   
     }
+    @Test
+    public void testIsGameWonHorizontalSecLine()
+    {   
+	final int X = 88;
+	final int Y = 79;
+	final int E = 32; //shorthand for empty    
+	TicTacLogic game = new TicTacLogic();
+	int[][] wonGame2 = {{E,E,E}, {X,X,X}, {E,E,E}};
+	game.board.setField(wonGame2);
+	assertEquals(true, game.isGameWon());
+    }
+    @Test
+    public void testIsGameWonHorizontalThirdLine()
+    {   
+	final int X = 88;
+	final int Y = 79;
+	final int E = 32; //shorthand for empty   
+	TicTacLogic game = new TicTacLogic(); 
+	int[][] wonGame3 = {{E,E,E}, {E,E,E}, {X,X,X}};
+	game.board.setField(wonGame3);
+	assertEquals(true, game.isGameWon());
+    }
+    @Test
+    public void testIsGameWonDiagonalNegativeSlope()
+    {
+	final int X = 88;
+	final int Y = 79;
+	final int E = 32; //shorthand for empty    
+	TicTacLogic game = new TicTacLogic();
+	int[][] wonGame1 = {{X,E,E}, {E,X,E}, {E,E,X}};
+	
+	game.board.setField(wonGame1);
+	assertEquals(true, game.isGameWon());
+	
+
+    }
+    @Test
+    public void testIsGameWonDiagonalPositiveSlope()
+    {
+	final int X = 88;
+	final int Y = 79;
+	final int E = 32; //shorthand for emptO   
+	TicTacLogic game = new TicTacLogic(); 
+	int[][] wonGame2 = {{E,E,X}, {E,X,E}, {X,E,E}};
+	game.board.setField(wonGame2);
+	assertEquals(true, game.isGameWon());
+    }
+
+    @Test
+    public void testNotWonGameAllEmpty()
+    {
+	final int X = 88;
+	final int O = 79;
+	final int E = 32; //shorthand for empty                                                        
+	TicTacLogic game = new TicTacLogic();
+	//create a bunch of scenarios                                                                  
+	
+	int[][] notWonGame1 = {{E,E,E}, {E,E,E}, {E,E,E}};
+	
+	//set the game field to our scenarios and ensure we are getting the right results              
+	game.board.setField(notWonGame1);
+	assertEquals(false, game.isGameWon());
+	   
+    }
+
+    @Test
+    public void testNotWonGameXShape()
+    {
+
+	final int X = 88;
+	final int O = 79;
+	final int E = 32; //shorthand for empty                                                        
+	TicTacLogic game = new TicTacLogic();
+	int[][] notWonGame = {{E,X,E}, {X,E,X}, {E,X,E}};
+	game.board.setField(notWonGame);
+	assertEquals(false, game.isGameWon());
+	
+    }
+    @Test
+    public void testNotWonGameThree()
+    {
+	final int X = 88;
+	final int O = 79;
+	final int E = 32; //shorthand for empty                                                        
+	TicTacLogic game = new TicTacLogic();
+	int[][] notWonGame3 = {{X,O,X}, {X,O,O}, {O,X,O}};
+	game.board.setField(notWonGame3);
+	assertEquals(false, game.isGameWon());
+	
+    }
+
 
 
 

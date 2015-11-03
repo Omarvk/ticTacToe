8be1a3700b2834d7ec56419e62668c14902ee336
Omar <killerbie@i-7f3b5dd9.csd07cloud.internal>
2015-11-03 13:17:24 +0000

    new post

--- src/main/java/com/btfc/tictactoe/Controller.java
+++ src/main/java/com/btfc/tictactoe/Controller.java
@@ -8,14 +8,19 @@ import java.awt.Point;
 
 public class Controller {
 	private static Map<String, Object> attri = new HashMap<>();
+	attri.put("Message","");
 	public Controller(final TicTacLogic tic) {
 		get("/",(req, res) ->	
 		{
+			// size of the board
 			int size = 3;
+			// loop the board
 			for(int x = 0; x < size; x++){
 				for(int y = 0; y < size; y++){
+					// get value of cell
 					int tala = tic.getSymbol(x, y);
 					String value = "";
+				   // change values into mark
 				    if( tala == 1)
 					{
 						value = "<img src=\"../graphics/red_x.svg\" id=\"i"+x+""+y+"\" alt=\"Cell "+x+"."+y+"\" />";
@@ -28,10 +33,9 @@ public class Controller {
 					{
 						value = "<img src=\"../graphics/empty.svg\" id=\"i"+x+""+y+"\" alt=\"Cell "+x+"."+y+"\" />";
 					}
-					//String value  = String.valueOf(tala);
+					// put string into hashmap for modelview
 					String cell = "a" + String.valueOf(x) + String.valueOf(y);
 					attri.put(cell, value);
-					System.out.println(cell+ " wat " +value );
 				}
 	
 			
@@ -41,7 +45,7 @@ public class Controller {
 
 		post("/move", (req, res) -> 
 		{
-		        //get the string labelled cell
+		    //get the val labelled cell from javascript
 			String cell = req.queryParams("cell");
 			//convert the 2nd and 3d characters to integers, the first one is useless
 			String px = Character.toString(cell.charAt(1));
@@ -49,10 +53,16 @@ public class Controller {
 			int x = Integer.parseInt(px);
 			int y = Integer.parseInt(py);
 			//here we need to do something smart with the point
-			tic.makeMove(x, y);
-			System.out.println("move: " + x + ", " + y);
+			String result;
+			result = tic.makeMove(x, y);
+			attri.put("Message", result);
 			return null;
 		});
+		post("/newGame", (req, res) ->
+		{
+			tic.newGame();
+			return null;
+		}
 	}
 	public static void main(String[] args) {
 		staticFileLocation("/public");
